2c9843904506378165e4f6c1a681b71454954cf9
cti-ttc <36923343+cti-ttc@users.noreply.github.com>
2018-02-28 18:49:50 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>TTC</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="dist/css/bootstrap-rtl.min.css" media="screen">
+    <link rel="stylesheet" href="demo/custom.min.css">
+    <link rel="stylesheet" href="demo/privet.css">
+	<link href="demo/awesome.min.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" sizes="32x32" href="favicon.ico">
+    <script></script>
+</head>
+<body class="rtl" style="">
+<div class="container">
+
+<div class="page-header" id="banner">
+            <div class="row">
+                <div class="col-lg-8 col-md-7 col-sm-6">
+                    <h1>مركز الموهوبين</h1>
+                    <p class="lead">كلية الاتصالات و تقنية المعلومات</p>
+                </div>
+            </div>
+        </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="login.php">تسجيل الدخول <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="archive.php">عرض اعمال المركز</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="register.php">انظم معنا</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="aboutus.php">تواصل معنا</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+<br />
