2a1d11b7e59cdc9a27c6f4bc400a01dfb236a399
W2AlharbiMe <w2.alharbi.me@gmail.com>
2018-03-03 22:41:39 +0300

    remove js folder

--- dist/js/client_validation.js
+++
@@ -1,8 +0,0 @@
-// CLIENT VALIDATION VERSION 1.3 
-var inputs = document.querySelectorAll("input.form-control");
-var spans = document.querySelectorAll("span.help-block");
-var btn = document.getElementById("validator");
-
-btn.addEventListener('click', function(event) {
-	// CODE ....
-});
