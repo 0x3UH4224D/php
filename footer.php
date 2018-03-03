f1aa321ab67a76e140bd5c9f1e35eb8b29a15d4a
W2AlharbiMe <w2.alharbi.me@gmail.com>
2018-03-03 22:37:21 +0300

    UPDATES

--- footer.php
+++ footer.php
@@ -1,1 +1,9 @@
 </div>
+<!-- import javascript -->
+<script src="/ttc/dist/js/jquery-3.2.1.slim.min.js"></script>
+<script src="/ttc/dist/js/popper.min.js"></script>
+<script src="/ttc/dist/js/bootstrap.min.js"></script>
+<script src="/ttc/dist/js/client_validation.js"></script>
+<script src="/ttc/dist/js/countDown.js"></script>
+</body>
+</html>
