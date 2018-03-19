d41b668bcf43e152aeafc9e50d3d53e25e045578
Muhannad Alrusayni <0x3UH4224D@gmail.com>
2018-03-19 20:35:40 +0300

    Add is*Used methods users table in the database class These methods does check if this data passed to them is already in the database or not

--- includes/Database/Database.php
+++ includes/Database/Database.php
@@ -2,10 +2,10 @@
 namespace CTG\Database;
 
 class Database {
-    private static $dbName = 'ctittc';
-    private static $dbHost = 'localhost';
-    private static $dbUsername = 'root';
-    private static $dbPassword = '1233456';
+    private const dbName = 'ctittc';
+    private const dbHost = 'localhost';
+    private const dbUsername = 'root';
+    private const dbPassword = '123456';
 
     // connection to the database
     private static $connection = null;
@@ -18,40 +18,51 @@ class Database {
     private const cannotCreateTable = "Couldn't create database table: ";
 
     // establish db connection
-    private function __construct() {
+    function __construct() {
         // if there is already an opened connection, increase our counter
         // and return.
-        if (!is_null(self::connection)) {
-            self::count++;
+        if (!is_null(self::$connection)) {
+            self::$count++;
             return;
         }
 
         // otherwise make a new connection
         try {
             // before we make connection we carete the database if it's not created
-            $this->createDatabaseIfNotExist();
-            self::connection = new \PDO(
-                "mysql:host=self::dbHost;dbname=self::dbName;",
+            $this->initDatabase();
+            $host = self::dbHost;
+            $name = self::dbName;
+            $dsn = "mysql:host=$host;dbname=$name;charset=utf8";
+            $opt = [
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                \PDO::ATTR_EMULATE_PREPARES => true
+            ];
+
+            self::$connection = new \PDO(
+                $dsn,
                 self::dbUsername,
-                self::dbPassword
+                self::dbPassword,
+                $opt
             );
+
             // increase out counter
-            $this->count++;
+            self::$count++;
         } catch (\PDOException $e) {
             // return an exception when errors happend
-            throw new \Exception($self::cannotConnectToDatabase);
+            throw new \Exception(self::cannotConnectToDatabase);
         }
     }
 
     // discconect from db
     function __destruct() {
         // decrease our counter
-        $this->count--;
+        self::$count--;
 
         // check if there are anyother user for the connection, if not disconnect
         // form the database
-        if ($this->count == 0) {
-            self::connection = null;
+        if (self::$count == 0) {
+            self::$connection = null;
         }
     }
 
@@ -65,14 +76,142 @@ class Database {
 
     private function initDatabase() {
         // TODO
+        $this->createDatabaseIfNotExist();
         $this->createTablesIfNotExist();
     }
 
-    protected function query($sql_statment) {
-        // TODO
+    protected function query(string $sql, $values) {
+        $stmt = self::$connection->prepare($sql);
+        $stmt->execute($values);
+        $rows = $stmt->fetchAll();
+        $stmt->closeCursor();
+        $stmt = null;
+        return $rows;
     }
 
-    function addUser($username, $hash_password, $email, $permission) {
+    protected function execute(string $sql, $values) {
+        $stmt = self::$connection->prepare($sql);
+        $result = $stmt->execute($values);
+        $stmt->closeCursor();
+        $stmt = null;
+        return $result;
+    }
+
+    // Methods for users table
+
+
+    // Check if username is already in use
+    function isUsernameUsed(string $username) {
+        $sql = 'SELECT username FROM users WHERE lower(username) = lower(?)';
+        $row = $this->query($sql, [$username]);
+
+        if (empty($row)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check if email is already in use
+    function isEmailUsed(string $email) {
+        $sql = 'SELECT email FROM users WHERE lower(email) = lower(?)';
+        $row = $this->query($sql, [$email]);
+
+        if (empty($row)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check if phone number is already in use
+    function isPhoneUsed(string $phone) {
+        $sql = 'SELECT phone FROM users WHERE phone = ?';
+        $row = $this->query($sql, [$phone]);
+
+        if (empty($row)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function isValidUsername(string $username) {
+        // TODO
+        return true;
+    }
+
+    function isValidPassword(string $password) {
+        // TODO
+        return true;
+    }
+
+    function isValidPhone(string $phone) {
+        // TODO
+        return true;
+    }
+
+    function isValidEmail(string $email) {
+        // TODO
+        return true;
+    }
+
+    function addUser(
+        string $username,
+        string $raw_password,
+        string $email,
+        string $phone = null
+    ) {
+        // check for input if they are in used or they are not valid
+        if ($this->isUsernameUsed($username)) {
+            throw new \Exception('This username is not available, please pick another one');
+        }
+        if (!$this->isValidUsername($username)) {
+            throw new \Exception('Username is not valid');
+        }
+
+        if ($this->isEmailUsed($email)) {
+            throw new \Exception('This email is not available, please pick another one');
+        }
+        if (!$this->isValidEmail($email)) {
+            throw new \Exception('Email is not valid');
+        }
+
+        if (!is_null($phone)) {
+            if ($this->isPhoneUsed((string)$phone)) {
+                throw new \Exception('This phone is not available, please pick another one');
+            }
+            if (!$this->isValidPhone((string)$phone)) {
+                throw new \Exception('phone is not valid');
+            }
+        }
+
+        if (!$this->isValidPassword($raw_password)) {
+            throw new \Exception('Password is not valid');
+        }
+
+
+        // TODO: generate a new slat to hash password, and save it in the database
+        $slat = 'SLAT_EMPTY';
+        $password = $raw_password;
+
+        // insert data to database
+        $sql = '';
+        $values = [];
+        if (is_null($phone)) {
+            $sql = 'INSERT INTO users (username, email, password, slat)'
+                 . 'VALUES (?, ?, ?, ?)';
+            $values = [$username, $email, $password, $slat];
+        } else {
+            $sql = 'INSERT INTO users (username, phone, email, password, slat)'
+                 . 'VALUES (?, ?, ?, ?, ?)';
+            $values = [$username, $phone, $email, $password, $slat];
+        }
+
+        return $this->execute($sql, $values);
+    }
+
+    function addAdmin($username, $raw_password, $email, $phone) {
         // TODO
     }
 
