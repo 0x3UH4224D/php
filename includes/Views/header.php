0f14645db2e32fc523ba96858fb6805cf66f4325
W2AlharbiMe <w2.alharbi.me@gmail.com>
2018-03-22 21:04:44 +0300

    update

--- includes/Views/header.php
+++ includes/Views/header.php
@@ -4,8 +4,16 @@
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- <link rel="Content-Type: text/css" href="theme/css/lib/bootstrap/bootstrap.css"> -->
+        
+        <!-- bootstrap version 4.0.0 <!check_update_bs> -->
         <link rel="stylesheet" href="theme/css/lib/bootstrap/bootstrap.css">
+
+        <!-- font-awesome version  5.0.8 <!check_update_fa> -->
+        <link rel="stylesheet" href="theme/css/lib/font-awesome/css/fontawesome.min.css">
+
+        <!-- custome style version 1.0.0 -->
+        <link rel="stylesheet" href="theme/css/main.css">
+
         <title><?php echo $this->title; ?></title>
     </head>
     <body>
--- index.php
+++ index.php
@@ -3,49 +3,93 @@ require_once './includes/Controllers/HomeController.php';
 require_once './includes/Router.php';
 
 use \CTG\Router;
-
-
-function css()
-{
-    header('Content-Type: text/css');
-    // main
-    // librarys
-    require_once './theme/css/lib/bootstrap/bootstrap.css';
-    require_once './theme/css/lib/font-awesome/css/fontawesome.min.css';
-    require_once './theme/css/lib/waves/waves.min.css';
-}
-
-function javascript()
-{
-    header('Content-Type: text/javascript');
-    // main
-    require_once './theme/js/countDown.js';
-    // library
-    require_once './theme/js/lib/bootstrap/jquery-3.2.1.slim.min.js';
-    require_once './theme/js/lib/bootstrap/bootstrap.min.js';
-}
-
-function popper()
-{
-    require_once './theme/js/lib/bootstrap/popper.min.js';
-}
-
 $router = new Router();
 
 // pages 
 $router->addPage('\CTG\Controllers\HomeController');
 
 
-// theme components
-    // css files
-$router->addRoute('/theme/css/lib/bootstrap/bootstrap.css', 'css');
-$router->addRoute('/theme/css/lib/font-awesome/css/fontawesome.min.css', 'css');
-$router->addRoute('/theme/css/lib/waves/waves.min.css', 'css');
-// javascript files
-$router->addRoute('/theme/js/lib/bootstrap/jquery-3.2.1.slim.min.js', 'javascript');
-$router->addRoute('/theme/js/lib/bootstrap/popper.min.js', 'popper');
-$router->addRoute('/theme/js/lib/bootstrap/bootstrap.min.js', 'javascript');
-$router->addRoute('/theme/js/countDown.js', 'javascript');
+
+
+/* theme components */
+# css files
+
+    // main css
+    function main_css()
+    {
+        header('Content-Type: text/css');
+        require_once './theme/css/main.css';
+    }
+    $router->addRoute('/theme/css/main.css', 'main_css');
+
+    // library
+
+    // wave css
+    function wave_css()
+    {
+        header('Content-Type: text/css');
+        require_once './theme/css/lib/bootstrap/bootstrap.css';
+        require_once './theme/css/lib/waves/waves.min.css';
+    }
+    $router->addRoute('/theme/css/lib/waves/waves.min.css', 'wave_css');
+
+    // bootstrap css
+    function bootstrap_css()
+    {
+        header('Content-Type: text/css');
+        require_once './theme/css/lib/bootstrap/bootstrap.css';
+    }
+    $router->addRoute('/theme/css/lib/bootstrap/bootstrap.css', 'bootstrap_css');
+
+    // font-awesome css
+    function font_awesome()
+    {
+        header('Content-Type: text/css');
+        require_once './theme/css/lib/font-awesome/css/fontawesome.min.css';
+    }
+    $router->addRoute('/theme/css/lib/font-awesome/css/fontawesome.min.css', 'font_awesome');
+    
+
+
+
+# javascript files
+
+    // main javascript
+    function countDown()
+    {
+        header('Content-Type: text/javascript');
+        require_once './theme/js/countDown.js';
+    }
+
+    $router->addRoute('/theme/js/countDown.js', 'countDown');
+
+    // library
+
+    // jquery slim 
+    function jquery_slim_js()
+    {
+        header('Content-Type: text/javascript');
+        require_once './theme/js/lib/bootstrap/jquery-3.2.1.slim.min.js';
+        
+    }
+    $router->addRoute('/theme/js/lib/bootstrap/jquery-3.2.1.slim.min.js', 'jquery_slim_js');
+
+    // bootstrap js
+    function bootstrap_js()
+    {
+        header('Content-Type: text/javascript');
+        require_once './theme/js/lib/bootstrap/bootstrap.min.js';
+    }
+    $router->addRoute('/theme/js/lib/bootstrap/bootstrap.min.js', 'bootstrap_js');
+
+    function popper()
+    {
+        header('Content-Type: text/javascript');
+        require_once './theme/js/lib/bootstrap/popper.min.js';
+    }
+    $router->addRoute('/theme/js/lib/bootstrap/popper.min.js', 'popper');
+
+    
 
 
 
