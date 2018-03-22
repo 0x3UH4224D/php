d84cdad10c4947fc4d9a47a35da793e03ed9ba33
W2AlharbiMe <w2.alharbi.me@gmail.com>
2018-03-22 20:12:44 +0300

    fixed theme

--- includes/Views/header.php
+++ includes/Views/header.php
@@ -1,9 +1,11 @@
+
 <!DOCTYPE html>
 <html lang="<?php echo $this->lang; ?>">
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="theme/css/main.css" type="text/css">
+        <!-- <link rel="Content-Type: text/css" href="theme/css/lib/bootstrap/bootstrap.css"> -->
+        <link rel="stylesheet" href="theme/css/lib/bootstrap/bootstrap.css">
         <title><?php echo $this->title; ?></title>
     </head>
     <body>
--- index.php
+++ index.php
@@ -3,14 +3,54 @@ require_once './includes/Controllers/HomeController.php';
 require_once './includes/Router.php';
 
 use \CTG\Router;
-function my_css()
+
+
+function css()
 {
-    require_once './theme/css/main.css';
+    header('Content-Type: text/css');
+    // main
+    // librarys
+    require_once './theme/css/lib/bootstrap/bootstrap.css';
+    require_once './theme/css/lib/font-awesome/css/fontawesome.min.css';
+    require_once './theme/css/lib/waves/waves.min.css';
 }
+
+function javascript()
+{
+    header('Content-Type: text/javascript');
+    // main
+    require_once './theme/js/countDown.js';
+    // library
+    require_once './theme/js/lib/bootstrap/jquery-3.2.1.slim.min.js';
+    require_once './theme/js/lib/bootstrap/bootstrap.min.js';
+}
+
+function popper()
+{
+    require_once './theme/js/lib/bootstrap/popper.min.js';
+}
+
 $router = new Router();
+
+// pages 
 $router->addPage('\CTG\Controllers\HomeController');
-$router->addRoute('/theme/css/main.css', 'my_css');
-// $router->addRoute('\theme\css\main.css', 'my_css');
+
+
+// theme components
+    // css files
+$router->addRoute('/theme/css/lib/bootstrap/bootstrap.css', 'css');
+$router->addRoute('/theme/css/lib/font-awesome/css/fontawesome.min.css', 'css');
+$router->addRoute('/theme/css/lib/waves/waves.min.css', 'css');
+// javascript files
+$router->addRoute('/theme/js/lib/bootstrap/jquery-3.2.1.slim.min.js', 'javascript');
+$router->addRoute('/theme/js/lib/bootstrap/popper.min.js', 'popper');
+$router->addRoute('/theme/js/lib/bootstrap/bootstrap.min.js', 'javascript');
+$router->addRoute('/theme/js/countDown.js', 'javascript');
+
+
+
+
+
 /* $router->addPage('\CTG\Pages\News');
  * $router->addPage('\CTG\Pages\AboutUs');
  * $router->addPage('\CTG\Pages\Clubs');
